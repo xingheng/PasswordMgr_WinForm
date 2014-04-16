9663013f900bc7098f99760114bdbb21b748c94a
Will Han <xingheng907@hotmail.com>
2014-04-16 21:40:14 +0800

    Updated project's setting and signature, etc.

--- PasswordMgr_WinForm/Properties/AssemblyInfo.cs
+++ PasswordMgr_WinForm/Properties/AssemblyInfo.cs
@@ -5,10 +5,10 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("PasswordMgr_WinForm")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("PasswordMgr")]
+[assembly: AssemblyDescription("A Password Management Tool")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Will Han")]
 [assembly: AssemblyProduct("PasswordMgr_WinForm")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
